7b3978a1791b254a08312049d48ebf3575f863e3
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-12-28 19:34:14 +0100

    create wallet funding functionality

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# About
+## About
 This is a bare-bones repo for a tutorial on working with Paystack's Webhooks.
+
+## Requirements
+1. NPM and Node.js installed.
+2. Web hosting (for the Webhook).
+3. Any API client (e.g Postman, Insomnia, Hoppscotch, Swagger). Postman will be used for this tutorial.
+4. A Paystack account, and secret API **test** keys (not to be mistaken for **live** keys).
+
--- app.js
+++ app.js
@@ -1,12 +1,63 @@
 const express = require('express')
+const cors = require('cors')
+const { default: axios } = require('axios')
 require('dotenv').config()
+const crypto = require('crypto')
 
 const app = express()
 const port = process.env.PORT
 
-// a default page
+app.use(express.json())
+app.use(cors())
+
+const initializationUrl = 'https://api.paystack.co/transaction/initialize'
+const headers = {
+  Authorization: `BEARER ${process.env.PAYSTACK_SECRET_KEY}`,
+  'Content-Type': 'application/json',
+}
+
+// default page
 app.get('/', (req, res) => {
   return res.send('Welcome to the Paystack tutorial')
+})
+
+// the route for processing a payment
+app.post('/fund', async (req, res) => {
+  const payload = {
+    email: req.body.email,
+    amount: req.body.amount
+  }
+
+  try {
+    const response = await axios.post(initializationUrl, payload, { headers })
+    return res.status(201).json({ status: true, data: response.data })
+  } catch(e) {
+    return res.status(400).json({ status: false, message: 'Funding failed' })
+  }
+})
+
+// the Webhook for receiving Paystack's response
+app.post('/paystack-webhook', async (req, res) => {
+  const paystackSignature = req.headers['x-paystack-signature']
+  const secret = process.env.PAYSTACK_SECRET_KEY
+
+  const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex')
+  if (hash == paystackSignature) {
+    
+    // Retrieve the request's body
+    const event = req.body
+    // Do something with the event ...
+  }
+
+  // You can log to the console to be sure
+  console.log('The Paystack webhook was called')
+
+  // A status response of 200 must be sent back to the Paystack 
+  res.status(200).json({
+    status: true,
+    message: 'Received Paystack Event'
+  });
+
 })
 
 // handling invalid routes
@@ -14,7 +65,7 @@ app.use('*', (req, res) => {
   return res.send('Invalid route')
 })
 
-// listen on your port of choice
+// listen on any env port of choice
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
 })
